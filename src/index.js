df7acefb0b9990e72f2e47cf898c79c2e19d2bfd
stella njeri ndanuko <sndanuko@gmail.com>
2025-02-08 13:48:04 +0300

    added local time

--- src/index.js
+++ src/index.js
@@ -24,6 +24,18 @@ function updateTime(params) {
       "h:mm:ss [<small>]A[</small>]"
     );
   }
+  //Sydney
+  let sydneyElement = document.querySelector("#sydney");
+  if (sydneyElement) {
+    let sydneyDateElement = sydneyElement.querySelector(".date");
+    let sydneyTimeElement = sydneyElement.querySelector(".time");
+    let sydneyTime = moment().tz("Australia/Sydney");
+
+    sydneyDateElement.innerHTML = sydneyTime.format("MMMM	Do YYYY");
+    sydneyTimeElement.innerHTML = sydneyTime.format(
+      "h:mm:ss [<small>]A[</small>]"
+    );
+  }
 }
 function updateCity(event) {
   let cityTimeZone = event.target.value;
